d564872590257f0ea80d339cc7ac29e4b780a364
kzkzzk <kzkzzk327@gmail.com>
2018-09-13 15:18:32 +0900

    fix other input

--- IAT-FORM/app/Http/Controllers/QuestionController.php
+++ IAT-FORM/app/Http/Controllers/QuestionController.php
@@ -42,13 +42,31 @@ class QuestionController extends Controller
         // 試行回数
         $time = $request->count;
         $block_id = $request->block_id;
-        foreach($request->q as $i => $q) {
-            $question = new Question;
-            $question->subject_id = $request->user()->id;
-            // 0から始まるので
-            $question->question_id = $block_id.'-'.($i + 1);
-            $question->answer = $q;
-            $question->save();
+        foreach($request->q as $i => $answers) {
+            if(is_array($answers)) {
+                foreach($answers as $answer) {
+                    $question = new Question;
+                    $question->subject_id = $request->user()->id;
+                    // 0から始まるので
+                    $question->question_id = $block_id.'-'.($i + 1);
+                    $question->answer = $answer;
+                    if($answer == 'on') {
+                        $question->answer = $request['textforscb'.($i + 1)];
+                    }
+                    $question->save();
+                }
+            } else {
+                $question = new Question;
+                $question->subject_id = $request->user()->id;
+                // 0から始まるので
+                $question->question_id = $block_id.'-'.($i + 1);
+                $question->answer = $answers;
+                if($answers == 'on') {
+                    $question->answer = $request['textforscb'.($i + 1)];
+                }
+                $question->save();
+            }
+            
         }
 
         $block_id++;
